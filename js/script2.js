6d7bdd61b7973d84b2a9319e73cb1ddc0e4b4d05
Muhadjir Fachrurradjie <adjiempandi@gmail.com>
2020-05-03 23:50:56 +0700

    Menambahkan detail

--- js/script2.js
+++ js/script2.js
@@ -63,13 +63,10 @@ function tampilkan() {
 					<div class="card-body">
 						<h2 class="card-title text-center"><strong>Kota Bima</strong></h2>
 						<p class="card-text">Total Positif : &nbsp;&nbsp;<strong>`+positif+`</strong></p>
-						<p class="card-text">PPTG &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;<strong>`+pptg+`</strong></p>
-						<p class="card-text">OTG &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;<strong>`+otg+`</strong></p>
-						<p class="card-text">ODP &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;<strong>`+odp+`</strong></p>
-						<p class="card-text">PDP &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;<strong>`+pdp+`</strong></p>
 						<p class="card-text">Dirawat &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;<strong>`+dirawat+`</strong></p>
 						<p class="card-text">Sembuh &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;<strong>`+sembuh+`</strong></p>
 						<p class="card-text" style="color: red">Meninggal &nbsp;: &nbsp;&nbsp;<strong>`+meninggal+`</strong></p>
+						<a href="#" class="btn btn-primary detail" data-toggle="modal" data-target="#exampleModal" data-id="kota_bima">Detail</a>
 					</div>
 				</div>
 			</div>`);
@@ -81,13 +78,10 @@ function tampilkan() {
 					<div class="card-body">
 						<h2 class="card-title text-center"><br><br><strong>Kabupaten Bima</strong></h2>
 						<p class="card-text">Total Positif : &nbsp;&nbsp;<strong>`+kab_positif+`</strong></p>
-						<p class="card-text">PPTG &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;<strong>`+kab_pptg+`</strong></p>
-						<p class="card-text">OTG &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;<strong>`+kab_otg+`</strong></p>
-						<p class="card-text">ODP &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;<strong>`+kab_odp+`</strong></p>
-						<p class="card-text">PDP &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;<strong>`+kab_pdp+`</strong></p>
 						<p class="card-text">Dirawat &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;<strong>`+kab_dirawat+`</strong></p>
 						<p class="card-text">Sembuh &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;<strong>`+kab_sembuh+`</strong></p>
 						<p class="card-text" style="color: red">Meninggal &nbsp;: &nbsp;&nbsp;<strong>`+kab_meninggal+`</strong></p>
+						<a href="#" class="btn btn-primary detail" data-toggle="modal" data-target="#exampleModal" data-id="kabupaten_bima">Detail</a>
 					</div>
 				</div>
 			</div>`);
@@ -99,13 +93,10 @@ function tampilkan() {
 					<div class="card-body">
 						<h2 class="card-title text-center"><strong>Kabupaten Dompu</strong></h2>
 						<p class="card-text">Total Positif : &nbsp;&nbsp;<strong>`+dom_positif+`</strong></p>
-						<p class="card-text">PPTG &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;<strong>`+dom_pptg+`</strong></p>
-						<p class="card-text">OTG &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;<strong>`+dom_otg+`</strong></p>
-						<p class="card-text">ODP &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;<strong>`+dom_odp+`</strong></p>
-						<p class="card-text">PDP &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;<strong>`+dom_pdp+`</strong></p>
 						<p class="card-text">Dirawat &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;<strong>`+dom_dirawat+`</strong></p>
 						<p class="card-text">Sembuh &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;<strong>`+dom_sembuh+`</strong></p>
 						<p class="card-text" style="color: red">Meninggal &nbsp;: &nbsp;&nbsp;<strong>`+dom_meninggal+`</strong></p>
+						<a href="#" class="btn btn-primary detail" data-toggle="modal" data-target="#exampleModal" data-id="kabupaten_dompu">Detail</a>
 					</div>
 				</div>
 			</div>`);
@@ -126,11 +117,12 @@ function tampilkan() {
 				<div class="card mb-4">
 					<div class="card-body">
 						<h2 class="card-title text-center"><strong>NTB</strong></h2>
+						<p class="card-text">Total Positif : &nbsp;&nbsp;<strong>`+ntb_positif+`</strong></p>
 						<p class="card-text">Dirawat : &nbsp;&nbsp;<strong>`+ntb_dirawat+`</strong></p>
 						<p class="card-text">Sembuh : &nbsp;&nbsp;<strong>`+ntb_sembuh+`</strong></p>
 						<p class="card-text" style="color: red">Meninggal : &nbsp;&nbsp;<strong>`+ntb_meninggal+`</strong></p>
-						<p class="card-text">Total Positif : &nbsp;&nbsp;<strong>`+ntb_positif+`</strong></p>
 						<p class="card-text">Total (PPTG+OTG+ODP+PDP) : &nbsp;&nbsp;<strong>`+ntb_keseluruhan+`</strong></p>
+						<a href="#" class="btn btn-primary detail" data-toggle="modal" data-target="#exampleModal" data-id="ntb">Detail</a>
 					</div>
 				</div>
 			</div>`);
@@ -149,3 +141,232 @@ function tampilkan() {
 }
 
 tampilkan();
+
+$('#tampil').on('click','.detail', function(){
+	let kom = $(this).data('id');
+	$.getJSON('https://corona.ntbprov.go.id/api/data', function(data) {
+
+		switch(kom) {
+		  	case 'kota_bima':
+		    var kota_bima = data.data.kota_bima;
+			var pptg = kota_bima.masih_pptg;
+			var selesai_pptg = kota_bima.selesai_pptg;
+			var otg = kota_bima.masih_otg;
+			var selesai_otg = kota_bima.selesai_otg;
+			var odp = kota_bima.masih_odp;
+			var selesai_odp = kota_bima.selesai_odp;
+			var pdp = kota_bima.masih_pdp;
+			var selesai_pdp = kota_bima.selesai_pdp;
+			var dirawat = kota_bima.dirawat;
+			var sembuh = kota_bima.sembuh;
+			var meninggal = kota_bima.meninggal;
+			var positif = kota_bima.sembuh + kota_bima.dirawat + kota_bima.meninggal;
+			var kota = 'Kota Bima';
+			var gam = 'img/kota_bima.png';
+		    break;
+		  	case 'kabupaten_bima':
+		    var kabupaten_bima = data.data.kabupaten_bima;
+			var pptg = kabupaten_bima.masih_pptg;
+			var selesai_pptg = kabupaten_bima.selesai_pptg;
+			var otg = kabupaten_bima.masih_otg;
+			var selesai_otg = kabupaten_bima.selesai_otg;
+			var odp = kabupaten_bima.masih_odp;
+			var selesai_odp = kabupaten_bima.selesai_odp;
+			var pdp = kabupaten_bima.masih_pdp;
+			var selesai_pdp = kabupaten_bima.selesai_pdp;
+			var dirawat = kabupaten_bima.dirawat;
+			var sembuh = kabupaten_bima.sembuh;
+			var meninggal = kabupaten_bima.meninggal;
+			var positif = kabupaten_bima.sembuh + kabupaten_bima.dirawat + kabupaten_bima.meninggal;
+			var kota = 'Kabupaten Bima';
+			var gam = 'img/kab_bima.png';
+		    break;
+		  	case 'kabupaten_dompu':
+		    var kabupaten_dompu = data.data.kabupaten_dompu;
+			var pptg = kabupaten_dompu.masih_pptg;
+			var selesai_pptg = kabupaten_dompu.selesai_pptg;
+			var otg = kabupaten_dompu.masih_otg;
+			var selesai_otg = kabupaten_dompu.selesai_otg;
+			var odp = kabupaten_dompu.masih_odp;
+			var selesai_odp = kabupaten_dompu.selesai_odp;
+			var pdp = kabupaten_dompu.masih_pdp;
+			var selesai_pdp = kabupaten_dompu.selesai_pdp;
+			var dirawat = kabupaten_dompu.dirawat;
+			var sembuh = kabupaten_dompu.sembuh;
+			var meninggal = kabupaten_dompu.meninggal;
+			var positif = kabupaten_dompu.sembuh + kabupaten_dompu.dirawat + kabupaten_dompu.meninggal;
+			var kota = 'Kabupaten Dompu';
+			var gam = 'img/dompu.png';
+		    break;
+		  	case 'ntb':
+		    var pptg = data.total_masih_pptg;
+		    var selesai_pptg = data.total_selesai_pptg;
+	  		var otg = data.total_masih_otg;
+	  		var selesai_otg = data.total_selesai_otg;
+	  		var odp = data.total_masih_odp;
+	  		var selesai_odp = data.total_selesai_odp;
+	  		var pdp = data.total_masih_pdp;
+	  		var selesai_pdp = data.total_selesai_pdp;
+	  		var dirawat = data.total_dirawat;
+	  		var sembuh = data.total_sembuh;
+	  		var meninggal = data.total_meninggal;
+	  		var ntb_keseluruhan = data.total_keseluruhan;
+	  		var positif = data.total_positif;
+			var kota = 'Nusa Tenggara Barat';
+			var gam = 'img/ntb.png';
+		    break;
+		  	default:
+		}
+			console.log(data);
+
+		$('.modal-body').html(`
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-4 text-center">
+						<img src="`+gam+`" height="150">
+					</div>
+
+					<div class="col-md-8">
+						
+						<p class="text-center"><strong>`+kota+`</strong></p>
+						<table>
+							<tr>
+								<td>
+									Total Positif
+								</td>
+								<td>
+									:
+								</td>
+								<td>
+									<strong>`+positif+`</strong> orang
+								</td>
+							</tr>
+							<tr>
+								<td>
+									PPTG (masih)
+								</td>
+								<td>
+									:
+								</td>
+								<td>
+									<strong>`+pptg+`</strong> orang
+								</td>
+							</tr>
+							<tr>
+								<td>
+									PPTG (selesai)
+								</td>
+								<td>
+									:
+								</td>
+								<td>
+									<strong>`+selesai_pptg+`</strong> orang
+								</td>
+							</tr>
+							<tr>
+								<td>
+									OTG (masih)
+								</td>
+								<td>
+									:
+								</td>
+								<td>
+									<strong>`+otg+`</strong> orang
+								</td>
+							</tr>
+							<tr>
+								<td>
+									OTG (selesai)
+								</td>
+								<td>
+									:
+								</td>
+								<td>
+									<strong>`+selesai_otg+`</strong> orang
+								</td>
+							</tr>
+							<tr>
+								<td>
+									ODP (masih)
+								</td>
+								<td>
+									:
+								</td>
+								<td>
+									<strong>`+odp+`</strong> orang
+								</td>
+							</tr>
+							<tr>
+								<td>
+									ODP (selesai)
+								</td>
+								<td>
+									:
+								</td>
+								<td>
+									<strong>`+selesai_odp+`</strong> orang
+								</td>
+							</tr>
+							<tr>
+								<td>
+									PDP (masih)
+								</td>
+								<td>
+									:
+								</td>
+								<td>
+									<strong>`+pdp+`</strong> orang
+								</td>
+							</tr>
+							<tr>
+								<td>
+									PDP (selesai)
+								</td>
+								<td>
+									:
+								</td>
+								<td>
+									<strong>`+selesai_pdp+`</strong> orang
+								</td>
+							</tr>
+							<tr>
+								<td>
+									Dirawat
+								</td>
+								<td>
+									:
+								</td>
+								<td>
+									<strong>`+dirawat+`</strong> orang
+								</td>
+							</tr>
+							<tr>
+								<td>
+									Sembuh
+								</td>
+								<td>
+									:
+								</td>
+								<td>
+									<strong>`+sembuh+`</strong> orang
+								</td>
+							</tr>
+							<tr style="color: red">
+								<td>
+									Meninggal
+								</td>
+								<td>
+									:
+								</td>
+								<td>
+									<strong>`+meninggal+`</strong> orang
+								</td>
+							</tr>
+						</table						
+					</div>
+				</div>
+			</div>
+		`);
+	});
+	
+})
